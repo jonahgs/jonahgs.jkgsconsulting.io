eb44153ea9a05616f17f1ba00799bf752079d838
jonahgreenbaumshinder <jonahkgs@gmail.com>
2025-02-13 15:44:46 -0500

    hero button qa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 // tailwind.config.js
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./*.html",
-    "./css/*.{css,html}",
-    "./js/**/*.{js,html}",
-  ],
+  mode: "jit",
+  content: ["./*.html", "./css/*.{css,html}", "./js/**/*.{js,html}"],
   theme: {
     // Bring back the typical Tailwind colors:
     colors: {
-      ...colors,   // This reintroduces e.g. green, yellow, gray, etc.
+      ...colors, // This reintroduces e.g. green, yellow, gray, etc.
     },
     // If you also need default spacing (p-4, m-8), text sizes, etc.:
     spacing: defaultTheme.spacing,
@@ -22,12 +19,12 @@ module.exports = {
     extend: {
       fontFamily: {
         // Keep Poppins plus the normal Tailwind sans stack
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
       // Any other custom additions...
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    "@tailwindcss/postcss"
+    ("@tailwindcss/forms")],
 };
